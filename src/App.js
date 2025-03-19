c753b7ba3ef06b08a215d2ddcb7e9732fc6c6551
bartoszK-zst <krol.bartosz@zst.radom.pl>
2025-03-19 14:32:21 +0100

    aaa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { RowsPhotoAlbum } from 'react-photo-album';
+import "react-photo-album/rows.css";
+import photos from './Components/Photos';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RowsPhotoAlbum photos={photos}/>
   );
 }
 
---
+++ src/Components/Photos.js
@@ -0,0 +1,11 @@
+import { Photo } from "react-photo-album";
+
+const photos = [
+    { src: "./photos/coffe-beans-1.jpg", width: 1280, height: 853 },
+    { src: "./photos/coffe-beans-2.jpg", width: 800, height: 600 },
+    { src: "./photos/coffe-beans-3.jpg", width: 800, height: 600 },
+    { src: "./photos/coffe-beans-4.jpg", width: 800, height: 600 },
+    { src: "./photos/coffe-beans-5.jpg", width: 800, height: 600 }
+  ];
+
+export default photos;
